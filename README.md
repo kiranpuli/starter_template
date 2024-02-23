a02c48781293ac5b29b4293604037998d09c1d60
kiranpuli <kiranpuli070899@gmail.com>
2024-02-23 22:19:34 +0530

    cleanup, package setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Starter template
+This serves as a starter template for web-apps using below tools
+* Build Tool
+    * Vite
+* Front-end
+    * React + TS
+* UI library
+    * NextUI
+    * Tailwind CSS
+* State management - TODO
+    * Redux
+* Routing - TODO
+    * react-router
+* API handling - TODO
+    * react-query
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="mx-auto grid w-1/2 place-items-center p-5">
+      <h1 className="p-5 text-2xl">
+        Starter template for Vite + React + NextUI
+      </h1>
+      <Button color="primary">More info</Button>
+    </div>
+  );
+}
